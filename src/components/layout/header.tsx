7c4daf021add402b9b31420323d52972d1031882
Mayank <mayanksharma4174@gmail.com>
2025-08-05 14:27:42 +0000

    Filmplex ka logo bada kro

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,10 +3,10 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center justify-center">
         <a href="/" className="flex items-center gap-2">
-          <div className="text-3xl font-bold flex items-center">
-            <span className="text-4xl text-gold-metallic">F</span>
-            <span className="text-3xl text-gold-metallic tracking-tighter scale-y-110">ILMPLE</span>
-            <span className="text-4xl text-gold-metallic">X</span>
+          <div className="text-5xl font-bold flex items-center">
+            <span className="text-6xl text-gold-metallic">F</span>
+            <span className="text-5xl text-gold-metallic tracking-tighter scale-y-110">ILMPLE</span>
+            <span className="text-6xl text-gold-metallic">X</span>
           </div>
         </a>
       </div>
