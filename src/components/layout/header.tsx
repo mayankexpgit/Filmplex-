fcf545a5181b4bfcb480ea90af65a72fd2f459ef
Mayank <mayanksharma4174@gmail.com>
2025-08-05 13:41:53 +0000

    Filmplex ka jo fon hai usko professional or bada look do or brighter

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,10 @@
-import { Clapperboard } from 'lucide-react';
-
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center justify-center">
         <a href="/" className="flex items-center gap-2">
-          <div className="flex items-center justify-center font-bold text-4xl">
-            <span className="text-gold-metallic text-5xl">F</span>
-            <span className="text-gold-metallic tracking-tighter scale-y-90">ILMPLE</span>
-            <span className="text-gold-metallic text-5xl">X</span>
+          <div className="flex items-center justify-center font-bold text-6xl">
+            <span className="text-gold-metallic tracking-wider">FILMPLEX</span>
           </div>
         </a>
       </div>
