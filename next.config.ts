6575c1b67f9f9f0630f563e3aec393c36409276d
Mayank <mayanksharma4174@gmail.com>
2025-08-05 17:45:29 +0000

    Abe anumati nahi duniya me jo bhi image link hai koi bhi dalu to show ka

--- next.config.ts
+++ next.config.ts
@@ -12,33 +12,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
+        hostname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'logos-world.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 's.yimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.edigitalagency.com.au',
-      },
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.*',
-        pathname: '**',
-      }
     ],
   },
 };
