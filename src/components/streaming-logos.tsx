861ed9c466fc3cc7a0c3d201517d1b82c0b26a8c
Mayank <mayanksharma4174@gmail.com>
2025-08-05 17:18:02 +0000

    Yrr bilkul badda nahi hua bahut bada karo or background se merge karo ap

--- src/components/streaming-logos.tsx
+++ src/components/streaming-logos.tsx
@@ -6,14 +6,14 @@ const StreamingLogos = () => {
     { name: 'Netflix', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png' },
     { name: 'Prime Video', url: 'https://logos-world.net/wp-content/uploads/2021/04/Amazon-Prime-Video-Logo-2022.png' },
     { name: 'Crunchyroll', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Crunchyroll_2024_stacked.svg/1200px-Crunchyroll_2024_stacked.svg.png' },
-    { name: 'Apple TV', url: 'https://s.yimg.com/ny/api/res/1.2/mdSF4Yl2.zsm2wueKGhg4g--/YXBwaWQ9aGlnaGxhbmRlcjt3PTI0MDA7aD0xMzUw/http://40.media.tumblr.com/4a5e4f836ed7f46340195905413c8ed0/tumblr_inline_nvk90iicmn1rxwctk_540.jpg' },
+    { name: 'Apple TV', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Apple_TV_logo_2022.svg/1024px-Apple_TV_logo_2022.svg.png' },
   ];
 
   return (
     <div className="py-4">
       <div className="flex justify-center items-center gap-6 md:gap-8 flex-wrap">
         {logos.map((logo) => (
-          <div key={logo.name} className="transition-all duration-300 ease-in-out transform hover:scale-105 relative h-12 w-36 sm:h-14 sm:w-40">
+          <div key={logo.name} className="transition-all duration-300 ease-in-out transform hover:scale-105 relative h-16 w-44 sm:h-20 sm:w-52">
             <Image
               src={logo.url}
               alt={`${logo.name} logo`}
