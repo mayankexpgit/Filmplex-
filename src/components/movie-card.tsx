9285777c003a03395735619470126a8b0251b1a2
Mayank <mayanksharma4174@gmail.com>
2025-08-14 09:16:52 +0000

    Suno 6 lines kisi kisi me thik se nahi show kar rha hai

--- src/components/movie-card.tsx
+++ src/components/movie-card.tsx
@@ -60,7 +60,7 @@ export default function MovieCard({ movie, variant = 'large' }: MovieCardProps) 
             </Badge>
           )}
         </div>
-        <div className="p-2 flex-grow flex flex-col justify-start h-24 overflow-hidden">
+        <div className="p-2 flex-grow flex flex-col justify-start h-32 overflow-hidden">
           {movie.cardInfoText ? (
              <p className="font-bold text-foreground text-sm leading-tight [display:-webkit-box] [-webkit-line-clamp:6] [-webkit-box-orient:vertical] overflow-hidden">
               {movie.cardInfoText}
