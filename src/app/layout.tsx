e9d8318f7fca6c936bbd5e1ed8e27257812630b9
Mayank <mayanksharma4174@gmail.com>
2025-08-30 07:48:42 +0000

    Mere public file me ye sab dala hai GitHub pe maine dala tha but nahi sh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,25 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'FILMPLEX',
   description: 'The future of cinema.',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', type: 'image/x-icon' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
