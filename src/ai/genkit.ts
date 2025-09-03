b12b135bbf2d0fbe5357f52113734e981aa74d97
Mayank <mayanksharma4174@gmail.com>
2025-09-03 10:39:46 +0000

    Bar bar wahi error arha hai sahi karne ke bad bhi

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,9 @@
 
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import { firebasePlugin } from '@genkit-ai/firebase';
+import firebase from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI(), firebasePlugin()],
+  plugins: [googleAI(), firebase()],
   model: 'googleai/gemini-2.0-flash',
 });
