fbdca460ac8919cde2ec58c546a372511bd3eb86
Mayank <mayanksharma4174@gmail.com>
2025-09-03 05:31:05 +0000

    Lakin bas click pe hi jump kyu kar rha hi hamesa karte rahne do bhale hi

--- src/app/movie/[slug]/page.tsx
+++ src/app/movie/[slug]/page.tsx
@@ -22,8 +22,8 @@ import FilmpilexLoader from '@/components/ui/filmplex-loader';
 const DownloadIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-        <polyline points="7 10 12 15 17 10" className="group-hover/button:animate-[bounce-y_1s_infinite]" />
-        <line x1="12" x2="12" y1="15" y2="3" className="group-hover/button:animate-[bounce-y_1s_infinite]" />
+        <polyline points="7 10 12 15 17 10" className="animate-[bounce-y_1s_infinite]" />
+        <line x1="12" x2="12" y1="15" y2="3" className="animate-[bounce-y_1s_infinite]" />
     </svg>
 );
 
