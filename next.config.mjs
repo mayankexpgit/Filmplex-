1b4337cd1526965a7ec9ec23f91ad615fcbd33b1
Mayank <mayanksharma4174@gmail.com>
2025-09-03 07:25:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,42 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'image.tmdb.org' },
-      { protocol: 'https', hostname: 'media.themoviedb.org' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
-      { protocol: 'https', hostname: 'logos-world.net' },
-      { protocol: 'https', hostname: 'www.edigitalagency.com.au' },
-      { protocol: 'https', hostname: 'placehold.co' },
-      { protocol: 'https', hostname: 'www.filmibeat.com' },
-      { protocol: 'https', hostname: 'assets-in.bmscdn.com' },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.edigitalagency.com.au',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.themoviedb.org',
+      },
+       {
+        protocol: 'https',
+        hostname: 'www.filmibeat.com',
+      },
+       {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets-in.bmscdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgs.search.brave.com',
+      }
     ],
   },
 };
