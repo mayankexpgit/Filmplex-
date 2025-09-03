fb14569d0f90853dc083d0c3183e610b7d86816d
Mayank <mayanksharma4174@gmail.com>
2025-09-03 08:35:00 +0000

    Lakin fir bhi error arhe hai

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,20 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**.tmdb.org' },
-      { protocol: 'https', hostname: '**.nflximg.net' },
-      { protocol: 'https', hostname: 'www.filmibeat.com' },
-      { protocol: 'https', hostname: '**.bmscdn.com' },
-      { protocol: 'https', hostname: '**.google.com' },
-      { protocol: 'https', hostname: '**.wikimedia.org' },
-      { protocol: 'https', hostname: '**.brave.com' },
-      { protocol: 'https', hostname: '**.media-amazon.com' },
-      { protocol: 'https', hostname: 'placehold.co' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
-      { protocol: 'https', hostname: 'logos-world.net' },
-      { protocol: 'https', hostname: 'www.edigitalagency.com.au' },
-    ],
+  experimental: {
+    serverComponentsExternalPackages: ["@genkit-ai/firebase"],
   },
-  experimental: {},
   devServer: {
     allowedDevOrigins: [
-      'https://studio.web.google.com',
-      'https://*.proxy.googleprod.com',
+      "https://vexel-cinema-develop.web.app",
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
 };
