b3b0579485ae6cd75137fa9456f76179c25fdd02
Mayank <mayanksharma4174@gmail.com>
2025-09-03 07:16:18 +0000

    Error wahi hai

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'www.edigitalagency.com.au',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.filmibeat.com',
       }
     ],
   },
