974faad1db906300c2d56da70fa3247100b62360
Mayank <mayanksharma4174@gmail.com>
2025-09-03 08:33:31 +0000

    Kya universal nahi ho sakti kya vercel me bhi na problem aye sara url ka

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,71 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@genkit-ai/firebase'],
+  },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'image.tmdb.org' },
-      { protocol: 'https', hostname: 'media.themoviedb.org' },
-      { protocol: 'https', hostname: 'www.filmibeat.com' },
-      { protocol: 'https', hostname: 'assets-in.bmscdn.com' },
-      { protocol: 'https', hostname: 'imgs.search.brave.com' },
-      { protocol: 'https', hostname: '*.google.com' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
-      { protocol: 'https', hostname: 'logos-world.net' },
-      { protocol: 'https', hostname: 'www.edigitalagency.com.au' },
-      { protocol: 'https', hostname: 'placehold.co' },
-      { protocol: 'https', hostname: 'dnm.nflximg.net' },
+      {
+        protocol: 'https',
+        hostname: '**.tmdb.org',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.nflximg.net',
+      },
+       {
+        protocol: 'https',
+        hostname: '*.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.filmibeat.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.bmscdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pics.filmaffinity.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgs.search.brave.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org'
+      },
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.edigitalagency.com.au'
+      }
     ],
   },
-  experimental: {},
   devServer: {
-    allowedDevOrigins: ['https://*.google.com'],
+    allowedDevOrigins: [
+      'https://firebase.studio',
+      'https://*.firebase.studio',
+      'https://*.web.app',
+      'https://*.firebaseapp.com',
+    ],
   },
 };
 
