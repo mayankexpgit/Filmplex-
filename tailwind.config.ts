f2100e9336f1b10d84ec2ed3bcb00300a5bf920b
Mayank <mayanksharma4174@gmail.com>
2025-09-03 05:17:04 +0000

    Kha animated ho rha hai

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -88,10 +89,21 @@ export default {
             height: '0',
           },
         },
+        'bounce-y': {
+          '0%, 100%': {
+            transform: 'translateY(-15%)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'bounce-y': 'bounce-y 1s infinite',
       },
     },
   },
