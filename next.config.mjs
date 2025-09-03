ec791a8c28335dd929eb36bea8e8cc9b68699559
Mayank <mayanksharma4174@gmail.com>
2025-09-03 08:39:27 +0000

    Fir wahi aise to bahut dikkat horhi hai

--- next.config.mjs
+++ next.config.mjs
@@ -1,55 +1,14 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const config = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-      },
-       {
-        protocol: 'https',
-        hostname: '**.tmdb.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'dnm.nflximg.net',
-      },
-       {
-        protocol: 'https',
-        hostname: '**.nflximg.net',
-      },
-       {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-      },
-       {
-        protocol: 'https',
-        hostname: '**.media-amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'logos-world.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.edigitalagency.com.au',
-      },
-       {
-        protocol: 'https',
-        hostname: '**.filmibeat.com',
+        hostname: '**',
       },
     ],
   },
-  experimental: {
-  },
 };
 
-export default nextConfig;
+export default config;
