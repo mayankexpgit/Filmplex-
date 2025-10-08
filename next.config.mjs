b1c5a10137ee0be1ddf193d9f1cc4e03488bdff3
Mayank <mayanksharma4174@gmail.com>
2025-10-08 06:47:08 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/w500/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.filmibeat.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.edigitalagency.com.au',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
   experimental: {
-    // This allows requests from the Firebase Studio development environment.
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
+    // This is to allow requests from the Firebase Studio development environment.
+    // It's a security feature in Next.js.
+    allowedDevOrigins: ["*.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev"],
   },
 };
 
