1f455c7eb1404941bc9be3781ae9f3e37f1981e4
Mayank <mayanksharma4174@gmail.com>
2025-10-08 06:48:37 +0000

    Suno nahi koi bhi image loading hone ka permission do bhale hi koi bhi i

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        allowedDevOrigins: ["6000-firebase-studio-1754397069489.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev"]
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'image.tmdb.org',
-            },
-            {
-                protocol: 'https',
-                hostname: 'www.filmibeat.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'upload.wikimedia.org',
-            },
-            {
-                protocol: 'https',
-                hostname: 'logos-world.net',
-            },
-            {
-                protocol: 'https',
-                hostname: 'www.edigitalagency.com.au',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            },
-            {
-                protocol: 'https',
-                hostname: 'assets-in.bmscdn.com',
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    // This is to allow the dev server to be accessed from cloud containers.
+    allowedDevOrigins: [
+      'https://*.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
