25b1583202dcc31b595a9b87e35adcf99e215224
Mayank <mayanksharma4174@gmail.com>
2025-10-08 10:55:09 +0000

    Or upar ka jo carousel hai 3 sec se 2 sec karo

--- src/components/movie-card-small.tsx
+++ src/components/movie-card-small.tsx
@@ -14,7 +14,7 @@ interface MovieCardSmallProps {
 
 const MovieCardSmall: FC<MovieCardSmallProps> = ({ movies }) => {
   const plugin = React.useRef(
-    Autoplay({ delay: 3000, stopOnInteraction: true })
+    Autoplay({ delay: 2000, stopOnInteraction: true })
   )
 
   return (
