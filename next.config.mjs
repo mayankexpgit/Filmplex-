16ffe6e2a0eef8611f8c37697f5742fcb51da6ed
Mayank <mayanksharma4174@gmail.com>
2025-10-08 06:47:50 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,50 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        port: '',
-        pathname: '/t/p/w500/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.filmibeat.com',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'logos-world.net',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.edigitalagency.com.au',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
-  experimental: {
-    // This is to allow requests from the Firebase Studio development environment.
-    // It's a security feature in Next.js.
-    allowedDevOrigins: ["*.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev"],
-  },
+    experimental: {
+        allowedDevOrigins: ["6000-firebase-studio-1754397069489.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev"]
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.filmibeat.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'upload.wikimedia.org',
+            },
+            {
+                protocol: 'https',
+                hostname: 'logos-world.net',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.edigitalagency.com.au',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+            {
+                protocol: 'https',
+                hostname: 'assets-in.bmscdn.com',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
