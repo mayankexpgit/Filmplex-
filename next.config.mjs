9f8cd28f436b92ba8d0dca27cac2e9ef4412a1e5
Mayank <mayanksharma4174@gmail.com>
2025-10-08 08:40:42 +0000

    Poster kyu nahi loading ho rha hai  maine kya bola tha koi bhi poster ur

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      "9000-firebase-studio-1754397069489.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev",
-    ],
-  },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.filmibeat.com',
-      },
       {
         protocol: 'https',
         hostname: 'image.tmdb.org',
@@ -20,7 +10,15 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
-       {
+      {
+        protocol: 'https',
+        hostname: 'www.filmibeat.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets-in.bmscdn.com',
+      },
+      {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
       },
@@ -31,7 +29,7 @@ const nextConfig = {
        {
         protocol: 'https',
         hostname: 'www.edigitalagency.com.au',
-      }
+      },
     ],
   },
 };
