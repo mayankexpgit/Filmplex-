704328e6eb766562b1643864ff3195837de37c51
Mayank <mayanksharma4174@gmail.com>
2025-10-12 10:08:19 +0000

    Pata nahi kyu  website jab maine vercel pe dala to tmdb ka jo  poster ha

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,16 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
       },
-    ],
-  },
-  experimental: {
-    // This is the correct place for allowedDevOrigins in newer Next.js versions.
-    // We will keep it here to avoid future cross-origin issues during development.
-    allowedDevOrigins: [
-      '9000-firebase-studio-1754397069489.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev',
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
