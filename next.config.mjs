c721358cc45ccfc02c3fc5828cd1b2e26c29e179
mayankexpgit <mayanksharma4174@gmail.com>
2025-10-12 15:20:54 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  experimental: {
-    // This is the correct place for allowedDevOrigins in newer Next.js versions.
-    // We will keep it here to avoid future cross-origin issues during development.
-    allowedDevOrigins: [
-      '9000-firebase-studio-1754397069489.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev',
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
     ],
   },
 };
