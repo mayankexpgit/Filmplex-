6b39a136ed65114b2b1d603cd162928614fab2fd
Mayank <mayanksharma4174@gmail.com>
2025-10-14 05:59:09 +0000

    To fir send push notification ka page kha hai

---
+++ src/app/admin/send-notifications/page.tsx
@@ -0,0 +1,12 @@
+
+import NotificationSender from '@/components/admin/send-notifications';
+
+export default function SendNotificationsPage() {
+  return (
+    <div className="container mx-auto py-8 md:py-12">
+      <div className="max-w-4xl mx-auto">
+        <NotificationSender />
+      </div>
+    </div>
+  );
+}
